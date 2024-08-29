7778fcce421f56389580ebca44ab3f3a54cbf574
icamdaf <icampoltek@gmail.com>
2024-08-29 08:41:30 +0700

    Jobsheet1

---
+++ JS01/Sepeda.java
@@ -0,0 +1,30 @@
+public class Sepeda {
+  private String merek;
+  private int kecepatan;
+  private int gear;
+
+  public void setMerk(String newValue) {
+    merek = newValue;
+  }
+
+  public void gantiGear(int newValue) {
+    gear = newValue;
+  }
+
+  public int tambahKecepatan(int increment) {
+    kecepatan += increment;
+    return kecepatan;
+  }
+
+  public int rem(int decrement) {
+    kecepatan -= decrement;
+    return kecepatan;
+  }
+
+  public void cekStatus() {
+    System.out.println("\n---------------------------------");
+    System.out.println("Merek: " + merek);
+    System.out.println("Kecepatan: " + kecepatan);
+    System.out.println("Gear: " + gear);
+  }
+}
---
+++ JS01/SepedaDemo.java
@@ -0,0 +1,29 @@
+
+public class SepedaDemo {
+  public static void main(String[] args) {
+    Sepeda spd1 = new Sepeda();
+    Sepeda spd2 = new Sepeda();
+    SepedaGunung spd3 = new SepedaGunung();
+
+    spd1.setMerk("Polygone/22");
+    spd1.tambahKecepatan(10);
+    spd1.gantiGear(2);
+    spd1.cekStatus();
+    System.out.println("---------------------------------");
+
+    spd2.setMerk("WiimCycle/22");
+    spd2.tambahKecepatan(10);
+    spd2.gantiGear(2);
+    spd2.tambahKecepatan(10);
+    spd2.gantiGear(3);
+    spd2.cekStatus();
+    System.out.println("---------------------------------");
+
+    spd3.setMerk("Family/22");
+    spd3.tambahKecepatan(9);
+    spd3.gantiGear(6);
+    spd3.setTipeSuspensi("Gas suspension");
+    spd3.cekStatus();
+    System.out.println("---------------------------------");
+  }
+}
